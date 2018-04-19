b03ede0f3c3c8dae92743a1bf9309e57cbc3014f
jmikell <jmikell@gmail.com>
2018-04-19 02:32:22 -0400

    working on list not done but close

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { DataService } from './services/data.service';
 
 // Specify entry components, module-level providers, etc. here.
 @NgModule({
-  providers: [],
+
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [DataService],
   entryComponents: []
 })
 export class AppExtrasModule { }
--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,7 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'my-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  private id: number;
+  private title: string;
+  private posts: Post[];
+
+  constructor(private dataService: DataService) {
+    console.log('made it in ds const');
+  }
+
+  public ngOnInit() {
+    console.log('ngoninit');
+    this.dataService.getPosts().subscribe((posts) => {
+      this.posts = posts;
+    console.log(this.posts[0].id);
+    });
+  }
+}
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
