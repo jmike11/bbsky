bf37030f269f726f215c83b1f94b9eb406d0cef4
jmikell <jmikell@gmail.com>
2018-04-20 10:55:35 -0400

    added services folder

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class DataService {
+
+  constructor(public http: Http) {
+    console.log('Data service connected...');
+   }
+
+   public getPosts() {
+    // fetch('https://jsonplaceholder.typicode.com/posts/')
+    // .then(response => response.json())
+    //  .then(json => console.log(json));
+
+     return this.http.get('https://jsonplaceholder.typicode.com/posts/')
+     .map(res => res.json());
+   }
+}
